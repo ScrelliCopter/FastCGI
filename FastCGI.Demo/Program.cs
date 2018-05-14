562f26cc7f154a8ce9e507884977f08625ac0464
Nick <ScrelliCopter@users.noreply.github.com>
2018-05-15 04:24:38 +1000

    Update project files for .NET Core 2.0.

--- FastCGI.Demo/Program.cs
+++ FastCGI.Demo/Program.cs
@@ -8,28 +8,31 @@ using FastCGI;
 
 namespace FastCGI.Demo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Create a new FCGIApplication, will accept FastCGI requests
-            var app = new FCGIApplication();
+	class Program
+	{
+		static void Main ( string[] args )
+		{
+			// Create a new FCGIApplication, will accept FastCGI requests
+			var app = new FCGIApplication();
 
-            // Handle requests by responding with a 'Hello World' message
-            app.OnRequestReceived += (sender, request) =>
-            {
-                var responseString =
-                      "HTTP/1.1 200 OK\n"
-                    + "Content-Type:text/html\n"
-                    + "\n"
-                    + "Hello World!";
+			// Handle requests by responding with a 'Hello World' message
+			app.OnRequestReceived += ( sender, request ) =>
+			{
+				StringBuilder responseString = new StringBuilder ();
+				//responseString.AppendLine ( "HTTP/1.1" );
+				responseString.AppendLine ( "Status: 200 OK" );
+				responseString.AppendLine ( "Content-Type: text/html" );
+				//responseString.AppendLine("charset=utf-8");
+				//responseString.AppendLine();
+				responseString.AppendLine ();
+				responseString.AppendLine ( "Hello World!" );
 
-                request.WriteResponseASCII(responseString);
-                request.Close();
-            };
+				request.WriteResponseASCII ( responseString.ToString () );
+				request.Close ();
+			};
 
-            // Start listening on port 19000
-            app.Run(19000);
-        }
-    }
+			// Start listening on port 19000
+			app.Run ( 19000 );
+		}
+	}
 }
